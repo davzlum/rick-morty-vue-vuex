3d7bb512f7c291ec82bd58013ee49992f4c59171
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-09-14 16:13:31 +0200

    Print characters on my list

--- rick-and-morty/src/components/ListCharacters.vue
+++ rick-and-morty/src/components/ListCharacters.vue
@@ -1,24 +1,30 @@
 <template>
   <section>
       <div class="characters">
-          <div class="characters__item">
-              <h2>Characters</h2>
+          <div class="characters__item" v-for="character in characters" :key="character.id">
+              {{ character.name }}
           </div>
       </div>
   </section>
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
     setup(){
         const store = useStore()
-
+        const characters = computed(()=>{
+            return store.state.charactersFilter
+        })
         onMounted(()=>{
             store.dispatch('getCharacters')
         })
+
+        return {
+            characters
+        }
     }
 }
 </script>
--- rick-and-morty/src/store/index.js
+++ rick-and-morty/src/store/index.js
@@ -18,7 +18,8 @@ export default createStore({
       try {
         const response = await fetch('https://rickandmortyapi.com/api/character');
         const data = await response.json();
-        console.log(data);
+        commit('setCharacters', data.results)
+        commit('setCharactersFilter', data.results)
       } catch (error) {
         console.error(error)
       }
