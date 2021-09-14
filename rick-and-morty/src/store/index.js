83aa00de7b185516743d8b78660b9294da198979
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-09-14 15:08:10 +0200

    Configure state and mutation to get data

--- rick-and-morty/src/store/index.js
+++ rick-and-morty/src/store/index.js
@@ -2,10 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters({commit}) {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const data = await response.json();
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
